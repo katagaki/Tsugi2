1867d88a803d4204f2a5c41b7ac6ccc2b4275c3e
katagaki <68231035+katagaki@users.noreply.github.com>
2022-04-02 10:57:41 +0800

    Add base UI.

---
+++ BusesFramework/BusesFramework.h
@@ -0,0 +1,18 @@
+//
+//  BusesFramework.h
+//  BusesFramework
+//
+//  Created by 堅書 on 2022/04/02.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BusesFramework.
+FOUNDATION_EXPORT double BusesFrameworkVersionNumber;
+
+//! Project version string for BusesFramework.
+FOUNDATION_EXPORT const unsigned char BusesFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BusesFramework/PublicHeader.h>
+
+
