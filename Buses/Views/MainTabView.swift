1d7072b7a4e7131795650f411050ba74113cfd7b
katagaki <68231035+katagaki@users.noreply.github.com>
2022-06-13 12:26:26 +0800

    Replace custom UI with sheet.

--- Buses/Views/MainTabView.swift
+++ Buses/Views/MainTabView.swift
@@ -10,43 +10,37 @@ import SwiftUI
 
 struct MainTabView: View {
     
+    @State var isSheetPresenting = true
     @State var coordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 1.30437, longitude: 103.82458), latitudinalMeters: 50000.0, longitudinalMeters: 50000.0)
     @State var userTrackingMode: MapUserTrackingMode = .follow
     
     var body: some View {
-        
-        GeometryReader { metrics in
-            ZStack(alignment: .bottomLeading) {
-                Map(coordinateRegion: $coordinateRegion,
-                    interactionModes: .all,
-                    showsUserLocation: true,
-                    userTrackingMode: $userTrackingMode)
-                .edgesIgnoringSafeArea(.all)
-                TabView {
-                    NearbyView()
-                        .tabItem {
-                            Label("TabTitle.Nearby", systemImage: "map.fill")
-                        }
-                    FavoritesView()
-                        .tabItem {
-                            Label("TabTitle.Favorites", systemImage: "star.fill")
-                        }
-                    DirectoryView()
-                        .tabItem {
-                            Label("TabTitle.Directory", systemImage: "book.closed.fill")
-                        }
-                    MoreView()
-                        .tabItem {
-                            Label("TabTitle.More", systemImage: "ellipsis")
-                        }
-                }
-                .mask {
-                    RoundedCornersShape(corners: [.topLeft, .topRight], radius: 12.0)
-                }
-                .edgesIgnoringSafeArea(.all)
-                .shadow(radius: 5.0)
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: metrics.size.height * 0.55)
+        Map(coordinateRegion: $coordinateRegion,
+            interactionModes: .all,
+            showsUserLocation: true,
+            userTrackingMode: $userTrackingMode)
+        .edgesIgnoringSafeArea(.all)
+        .sheet(isPresented: $isSheetPresenting) {
+            TabView {
+                NearbyView()
+                    .tabItem {
+                        Label("TabTitle.Nearby", systemImage: "map.fill")
+                    }
+                FavoritesView()
+                    .tabItem {
+                        Label("TabTitle.Favorites", systemImage: "star.fill")
+                    }
+                DirectoryView()
+                    .tabItem {
+                        Label("TabTitle.Directory", systemImage: "book.closed.fill")
+                    }
+                MoreView()
+                    .tabItem {
+                        Label("TabTitle.More", systemImage: "ellipsis")
+                    }
             }
+            .interactiveDismissDisabled(true)
+            .presentationDetents([.medium, .large])
         }
     }
 }
@@ -54,17 +48,5 @@ struct MainTabView: View {
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainTabView()
-    }
-}
-
-struct RoundedCornersShape: Shape {
-    let corners: UIRectCorner
-    let radius: CGFloat
-    
-    func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(roundedRect: rect,
-                                byRoundingCorners: corners,
-                                cornerRadii: CGSize(width: radius, height: radius))
-        return Path(path.cgPath)
     }
 }
