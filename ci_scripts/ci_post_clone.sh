28f4dfb74043df2685d9406cfdb316f40eb7360b
katagaki <68231035+katagaki@users.noreply.github.com>
2023-03-03 14:02:08 +0800

    Create Xcode Cloud CI script.

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Buses 2
+#
+#  Created by 堅書 on 3/3/23.
+#
+
+set -e
+
+echo "Appending LTA DataMall API key to APIKeys.plist."
+echo $APIKEY_LTADATAMALL
+
+cd Buses
+plutil -create xml1 APIKeys.plist
+plutil -insert LTA -string $APIKEY_LTADATAMALL APIKeys.plist
+
+cat APIKeys.plist
+
+echo "Post-clone script completed."
+exit 0
