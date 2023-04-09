a677af4a2eb89b58a8792587712edd52c48cfb16
katagaki <68231035+katagaki@users.noreply.github.com>
2023-04-09 23:29:17 +0800

    Fix logic for detecting version upgrade/downgrade.

--- Buses/Utilities/Global/Defaults.swift
+++ Buses/Utilities/Global/Defaults.swift
@@ -10,15 +10,13 @@ import Foundation
 let defaults = UserDefaults.standard
 
 func initializeDefaultConfiguration() {
-    if defaults.value(forKey: "CurrentVersion") == nil {
-        defaults.setValue("\(versionNumber).\(buildNumber)", forKey: "CurrentVersion")
-    } else {
-        if let previouslyDetectedVersion = defaults.string(forKey: "CurrentVersion"),
-           previouslyDetectedVersion != "\(versionNumber).\(buildNumber)" {
-            log("App was either updated or downgraded! Previously detected version was \(previouslyDetectedVersion), current version is \(versionNumber).\(buildNumber).")
-            // Perform future schema/data updates here
-        }
+    if defaults.value(forKey: "CurrentVersion") != nil,
+       let previouslyDetectedVersion = defaults.string(forKey: "CurrentVersion"),
+       previouslyDetectedVersion != "\(versionNumber).\(buildNumber)" {
+        log("App was either updated or downgraded! Previously detected version was \(previouslyDetectedVersion), current version is \(versionNumber).\(buildNumber).")
+        // Perform future schema/data updates here
     }
+    defaults.setValue("\(versionNumber).\(buildNumber)", forKey: "CurrentVersion")
     if defaults.value(forKey: "StartupTab") == nil {
         defaults.set(0, forKey: "StartupTab")
     }
