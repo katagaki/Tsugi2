5bac329899c0d55156ecce54737aeac041ac3595
katagaki <68231035+katagaki@users.noreply.github.com>
2023-04-11 22:44:20 +0800

    Move LocationDelegate out to its own file.

--- Buses/Views/Nearby/NearbyView.swift
+++ Buses/Views/Nearby/NearbyView.swift
@@ -210,27 +210,3 @@ struct NearbyView_Previews: PreviewProvider {
     static func showToast(message: String, type: ToastType = .None, hideAutomatically: Bool = true) async { }
     
 }
-
-class LocationDelegate: NSObject, ObservableObject, CLLocationManagerDelegate {
-
-    var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 1.354454, longitude: 103.946362),
-                                    latitudinalMeters: 250.0,
-                                    longitudinalMeters: 250.0)
-    var completion: () -> Void = {}
-    
-    @Published var authorizationStatus: CLAuthorizationStatus?
-    
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        authorizationStatus = manager.authorizationStatus
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        region.center.latitude = (manager.location?.coordinate.latitude)!
-        region.center.longitude = (manager.location?.coordinate.longitude)!
-        log("Updated location.")
-        manager.stopUpdatingLocation()
-        log("Calling completion handler in location delegate.")
-        completion()
-    }
-    
-}
