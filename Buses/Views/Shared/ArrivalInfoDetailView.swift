8d60a62c8d4f31703ae5bbdcb1eb9dec946adb0f
katagaki <68231035+katagaki@users.noreply.github.com>
2023-04-14 22:43:18 +0800

    Fix setting notification when in custom data mode.

--- Buses/Views/Shared/ArrivalInfoDetailView.swift
+++ Buses/Views/Shared/ArrivalInfoDetailView.swift
@@ -172,31 +172,47 @@ struct ArrivalInfoDetailView: View {
                 } else {
                     // TODO: Fix setting of notification when mode is FavoriteLocationCustomData
                     // TODO: Remove .constant() setting for bus stop
-                    if let busStop = self.busStop {
-                        let content = UNMutableNotificationContent()
-                        let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.weekday, .hour, .minute, .second],
-                                                                                                                  from: date - (2 * 60)), repeats: false)
-                        content.title = localized("Notification.Arriving.Title").replacingOccurrences(of: "%1", with: busStop.wrappedValue.description ?? localized("Shared.BusStop.Description.None"))
-                        content.body = localized("Notification.Arriving.Description").replacingOccurrences(of: "%s1", with: busService.serviceNo).replacingOccurrences(of: "%s2", with: date.formatted(date: .omitted, time: .shortened))
-                        content.userInfo = ["busService": busService.serviceNo,
-                                            "stopCode": busStop.wrappedValue.code,
-                                            "stopDescription": busStop.wrappedValue.description ?? localized("Shared.BusStop.Description.None")]
-                        content.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "Ding.caf"))
-                        content.interruptionLevel = .timeSensitive
-                        center.add(UNNotificationRequest(identifier: "\(busStop.wrappedValue.code).\(busService.serviceNo).\(date.formatted(date: .numeric, time: .shortened))",
-                                                         content: content,
-                                                         trigger: trigger)) { (error) in
-                            if let error = error {
-                                log("Error occurred while setting notifications: \(error.localizedDescription)")
-                                toaster.showToast(localized("Notification.Error"),
-                                                        type: .Exclamation,
-                                                        hideAutomatically: true)
-                            } else {
-                                log("Notification set with content: \(content.body), and will appear at \((date - (2 * 60)).formatted(date: .complete, time: .complete)).")
-                                toaster.showToast(localized("Notification.Set"),
-                                                        type: .Checkmark,
-                                                        hideAutomatically: true)
-                            }
+                    let content = UNMutableNotificationContent()
+                    let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.weekday, .hour, .minute, .second],
+                                                                                                              from: date - (2 * 60)), repeats: false)
+                    var identifier = ""
+                    switch mode {
+                    case .BusStop, .FavoriteLocationLiveData, .NotificationItem:
+                        if let busStop = busStop {
+                            content.title = localized("Notification.Arriving.Title").replacingOccurrences(of: "%1", with: busStop.wrappedValue.description ?? localized("Shared.BusStop.Description.None"))
+                            content.body = localized("Notification.Arriving.Description").replacingOccurrences(of: "%s1", with: busService.serviceNo).replacingOccurrences(of: "%s2", with: date.formatted(date: .omitted, time: .shortened))
+                            content.userInfo = ["busService": busService.serviceNo,
+                                                "stopCode": busStop.wrappedValue.code,
+                                                "stopDescription": busStop.wrappedValue.description ?? localized("Shared.BusStop.Description.None")]
+                            content.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "Ding.caf"))
+                            content.interruptionLevel = .timeSensitive
+                            identifier = "\(busStop.wrappedValue.code).\(busService.serviceNo).\(date.formatted(date: .numeric, time: .shortened))"
+                        }
+                    case .FavoriteLocationCustomData:
+                        if let favoriteLocation = favoriteLocation?.wrappedValue {
+                            content.title = localized("Notification.Arriving.Title").replacingOccurrences(of: "%1", with: favoriteLocation.nickname ?? localized("Shared.BusStop.Description.None"))
+                            content.body = localized("Notification.Arriving.Description").replacingOccurrences(of: "%s1", with: busService.serviceNo).replacingOccurrences(of: "%s2", with: date.formatted(date: .omitted, time: .shortened))
+                            content.userInfo = ["busService": busService.serviceNo,
+                                                "stopCode": busService.busStopCode ?? "00000",
+                                                "stopDescription": favoriteLocation.nickname ?? localized("Shared.BusStop.Description.None")]
+                            content.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "Ding.caf"))
+                            content.interruptionLevel = .timeSensitive
+                            identifier = "\(busService.busStopCode ?? "00000").\(busService.serviceNo).\(date.formatted(date: .numeric, time: .shortened))"
+                        }
+                    }
+                    center.add(UNNotificationRequest(identifier: identifier,
+                                                     content: content,
+                                                     trigger: trigger)) { (error) in
+                        if let error = error {
+                            log("Error occurred while setting notifications: \(error.localizedDescription)")
+                            toaster.showToast(localized("Notification.Error"),
+                                                    type: .Exclamation,
+                                                    hideAutomatically: true)
+                        } else {
+                            log("Notification set with content: \(content.body), and will appear at \((date - (2 * 60)).formatted(date: .complete, time: .complete)).")
+                            toaster.showToast(localized("Notification.Set"),
+                                                    type: .Checkmark,
+                                                    hideAutomatically: true)
                         }
                     }
                 }
