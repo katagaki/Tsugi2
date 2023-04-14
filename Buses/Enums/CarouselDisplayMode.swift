28de983ee6d72b0650be488dee73f0ea1ae70110
katagaki <68231035+katagaki@users.noreply.github.com>
2023-04-14 23:10:35 +0800

    Optimize carousel display mode enum.

--- Buses/Enums/CarouselDisplayMode.swift
+++ Buses/Enums/CarouselDisplayMode.swift
@@ -8,6 +8,40 @@
 import Foundation
 
 enum CarouselDisplayMode: String {
+    
+    func fontSize() -> Double {
+        switch self {
+        case .Full:
+            return 24.0
+        case .Small:
+            return 20.0
+        case .Minimal:
+            return 16.0
+        }
+    }
+    
+    func cornerRadius() -> Double {
+        switch self {
+        case .Full:
+            return 10.0
+        case .Small:
+            return 8.0
+        case .Minimal:
+            return 6.0
+        }
+    }
+    
+    func width() -> Double {
+        switch self {
+        case .Full:
+            return 80.0
+        case .Small:
+            return 72.0
+        case .Minimal:
+            return 56.0
+        }
+    }
+    
     case Full = "Full"
     case Small = "Small"
     case Minimal = "Minimal"
